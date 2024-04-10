e1c1127f09303a4c5cf1f5ed1c99751e38d90616
JosiMacGyver <joseana.stos.soares@gmail.com>
2024-04-10 15:02:41 -0300

    starting repository

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone: true,
+  imports:[
+    HomeComponent,
+    RouterModule
+  ],
+  styleUrls: ['./app.component.scss'],
+  template: `
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+`,
 })
 export class AppComponent {
-  title = 'land';
+  title = 'homes';
 }
